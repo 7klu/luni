d46f555d6b81a94d6354bc97b6c1b0d3a13edb41
x7klu <debianlunar.sh@gmail.com>
2025-02-24 22:34:46 +0100

    linux_dev update

--- lunilib/dev/linux_dev.c
+++ lunilib/dev/linux_dev.c
@@ -1,134 +1,262 @@
 #include "./luni_dev.h"
 
 /*
- - write ! x86, x86_64, arm, aarch64
- - read  ! x86, x86_64, arm, aarch64
+ - write ! x86, x86_64, arm, aarch64, riscv64, powerpc64, mips, sparc64
+ - read  ! x86, x86_64, arm, aarch64, riscv64, powerpc64, mips, sparc64
 */
 
-ssize_t linux_x86_write(int fd, const void *buff, size_t count) {
+ssize_t linux_x86_write(int fd, const void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
-        "movl $4, %%eax;"
-        "movl %1, %%ebx;"
-        "movl %2, %%ecx;"
-        "movl %3, %%edx;"
-        "int $0x80;"
-        "movl %%eax, %0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "%eax", "%ebx", "%ecx", "%edx"
-    );
+    __asm__ volatile(
+        "movl $4, %%eax;" // load syscall write (4) into eax
+        "movl %1, %%ebx;" // move fd into ebx
+        "movl %2, %%ecx;" // move buff into ecx
+        "movl %3, %%edx;" // move count into edx
+        "int $0x80;"      // invoke syscall
+        "movl %%eax, %0;" // store syscall ret value (eax) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "%eax", "%ebx", "%ecx", "%edx");
     return ret;
 }
 
-ssize_t linux_x86_64_write(int fd, const void *buff, size_t count) {
+ssize_t linux_x86_64_write(int fd, const void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
-        "movq $1, %%rax;"
-        "movq %1, %%rdi;"
-        "movq %2, %%rsi;"
-        "movq %3, %%rdx;"
-        "syscall;"
-        "movq %%rax, %0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "%rax", "%rdi", "%rsi", "%rdx"
-    );
+    __asm__ volatile(
+        "movq $1, %%rax;" // load syscall write (1) into rax
+        "movq %1, %%rdi;" // move fd into rdi
+        "movq %2, %%rsi;" // move buff into rsi
+        "movq %3, %%rdx;" // move count into rdx
+        "syscall;"        // invoke syscall
+        "movq %%rax, %0;" // store syscall ret value (rax) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "%rax", "%rdi", "%rsi", "%rdx");
     return ret;
 }
 
-ssize_t linux_arm_write(int fd, const void *buff, size_t count) {
+ssize_t linux_arm_write(int fd, const void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
-        "mov r7, #4;"
-        "mov r0, %1;"
-        "mov r1, %2;"
-        "mov r2, %3;"
-        "swi #0;"
-        "mov %0, r0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "r7", "r0", "r1", "r2"
-    );
+    __asm__ volatile(
+        "mov r7, #4;" // laod syscall write (4) into r7
+        "mov r0, %1;" // move fd into r0
+        "mov r1, %2;" // move buff into r1
+        "mov r2, %3;" // move count into r2
+        "swi #0;"     // invoke syscall
+        "mov %0, r0;" // store syscall ret value (r0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "r7", "r0", "r1", "r2");
     return ret;
 }
 
-ssize_t linux_aarch64_write(int fd, const void *buff, size_t count) {
+ssize_t linux_aarch64_write(int fd, const void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
-        "mov x8, #64;"
-        "mov x0, %1;"
-        "mov x1, %2;"
-        "mov x2, %3;"
-        "svc #0;"
-        "mov %0, x0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "x8", "x0", "x1", "x2"
-    );
+    __asm__ volatile(
+        "mov x8, #64;" // laod syscall write (64) into x0
+        "mov x0, %1;"  // move fd into x0
+        "mov x1, %2;"  // move buff into x1
+        "mov x2, %3;"  // move count into x2
+        "svc #0;"      // invoke syscall
+        "mov %0, x0;"  // store syscall ret value (x0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "x8", "x0", "x1", "x2");
     return ret;
 }
 
-ssize_t linux_x86_read(int fd, void *buff, size_t count) {
+ssize_t linux_riscv64_write(int fd, const void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
+    __asm__ volatile(
+        "li a7, 64;" // laod syscall write (64) into a7
+        "mv a0, %1;" // move fd into a0
+        "mv a1, %2;" // move buff into a1
+        "mv a2, %3;" // move count into a2
+        "ecall;"     // invoke syscall
+        "mv %0, a0;" // store syscall ret value (a0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "a7", "a0", "a1", "a2");
+    return ret;
+}
+
+ssize_t linux_powerpc64_write(int fd, const void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "li r0, 4;"  // load syscall write (4) into r0
+        "mr r3, %1;" // move fd into r3
+        "mr r4, %2;" // move buff into r4
+        "mr r5, %3;" // move count into r5
+        "sc;"        // invoke syscall
+        "mr %0, r3;" // store syscall ret value (r3) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "r0", "r3", "r4", "r5");
+    return ret;
+}
+
+ssize_t linux_mips_write(int fd, const void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "li $v0, 4004;" // load syscall write (4004) into $v0
+        "move $a0, %1;" // move fd into $a0
+        "move $a1, %2;" // move buff into $a1
+        "move $a2, %3;" // move count into $a2
+        "syscall;"      // invoke syscall
+        "move %0, $v0;" // store syscall ret value ($v0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "$v0", "$a0", "$a1", "$a2");
+    return ret;
+}
+
+ssize_t linux_sparc64_write(int fd, const void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "mov 64, %%g1;" // load syscall write (64) into g1
+        "mov %1, %%o0;" // move fd into o0
+        "mov %2, %%o1;" // move buff into o1
+        "mov %3, %%o2;" // move count into o2
+        "ta 0;"         // invoke syscall
+        "mov %%o0, %0;" // store syscall ret value (o0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "%g1", "%o0", "%o1", "%o2");
+    return ret;
+}
+
+ssize_t linux_x86_read(int fd, void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
         "movl $3, %%eax;"
         "movl %1, %%ebx;"
         "movl %2, %%ecx;"
         "movl %3, %%edx;"
         "int $0x80;"
         "movl %%eax, %0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "%eax", "%ebx", "%ecx", "%edx"
-    );
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "%eax", "%ebx", "%ecx", "%edx");
     return ret;
 }
 
-ssize_t linux_x86_64_read(int fd, void *buff, size_t count) {
+ssize_t linux_x86_64_read(int fd, void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
+    __asm__ volatile(
         "movq $0, %%rax;"
         "movq %1, %%rdi;"
         "movq %2, %%rsi;"
         "movq %3, %%rdx;"
         "syscall;"
         "movq %%rax, %0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "%rax", "%rdi", "%rsi", "%rdx"
-    );
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "%rax", "%rdi", "%rsi", "%rdx");
     return ret;
 }
 
-ssize_t linux_arm_read(int fd, void *buff, size_t count) {
+ssize_t linux_arm_read(int fd, void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
+    __asm__ volatile(
         "mov r7, #3;"
         "mov r0, %1;"
         "mov r1, %2;"
         "mov r2, %3;"
         "swi #0;"
         "mov %0, r0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "r7", "r0", "r1", "r2"
-    );
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "r7", "r0", "r1", "r2");
     return ret;
 }
 
-ssize_t linux_aarch64_read(int fd, void *buff, size_t count) {
+ssize_t linux_aarch64_read(int fd, void *buff, size_t count)
+{
     ssize_t ret;
-    __asm__ volatile (
+    __asm__ volatile(
         "mov x8, #63;"
         "mov x0, %1;"
         "mov x1, %2;"
         "mov x2, %3;"
         "svc #0;"
         "mov %0, x0;"
-        : "=r" (ret)
-        : "r" (fd), "r" (buff), "r" (count)
-        : "x8", "x0", "x1", "x2"
-    );
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "x8", "x0", "x1", "x2");
+    return ret;
+}
+
+ssize_t linux_riscv64_read(int fd, void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "li a7, 63;"
+        "mv a0, %1;"
+        "mv a1, %2;"
+        "mv a2, %3;"
+        "ecall;"
+        "mv %0, a0;"
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "a7", "a0", "a1", "a2");
+    return ret;
+}
+
+ssize_t linux_powerpc64_read(int fd, void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "li r0, 3;"  // load syscall read (3) into r0
+        "mr r3, %1;" // move fd into r3
+        "mr r4, %2;" // move buff into r4
+        "mr r5, %3;" // move count into r5
+        "sc;"        // invoke syscall
+        "mr %0, r3;" // store syscall ret value (r3) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "r0", "r3", "r4", "r5");
+    return ret;
+}
+
+ssize_t linux_mips_read(int fd, void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "li $v0, 4003;" // load syscall read (4003) into $v0
+        "move $a0, %1;" // move fd into $a0
+        "move $a1, %2;" // move buff into $a1
+        "move $a2, %3;" // move count into $a2
+        "syscall;"      // invoke syscall
+        "move %0, $v0;" // store syscall ret value ($v0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "$v0", "$a0", "$a1", "$a2");
+    return ret;
+}
+
+ssize_t linux_sparc64_read(int fd, void *buff, size_t count)
+{
+    ssize_t ret;
+    __asm__ volatile(
+        "mov 63, %%g1;" // load syscall read (63) into g1
+        "mov %1, %%o0;" // move fd into o0
+        "mov %2, %%o1;" // move buff into o1
+        "mov %3, %%o2;" // move count into o2
+        "ta 0;"         // invoke syscall
+        "mov %%o0, %0;" // store syscall ret value (o0) into ret
+        : "=r"(ret)
+        : "r"(fd), "r"(buff), "r"(count)
+        : "%g1", "%o0", "%o1", "%o2");
     return ret;
 }
--- lunilib/dev/luni_dev.h
+++ lunilib/dev/luni_dev.h
@@ -1,68 +1,68 @@
-/*
-path: luni/lunilib
-*/
-
 #ifndef LUNI_DEV_H
 #define LUNI_DEV_H
 
-#if defined(_WIN32) || defined(_WIN64) // windows
-    #ifdef _WIN64
-        #define __c_write win_x86_64_write
-        #define __c_read win_x86_64_read
-    #else
-        #define __c_write win_x86_write
-        #define __c_read win_x86_read
-    #endif
-#elif defined(__linux__) // all linux distro
-    #ifdef __x86_64__
-        #define __c_write linux_x86_64_write
-        #define __c_read linux_x86_64_read
-    #elif defined(__i386__)
-        #define __c_write linux_x86_write
-        #define __c_read linux_x86_read
-    #elif defined(__arm__)
-        #define __c_write linux_arm_write
-        #define __c_read linux_arm_read
-    #elif defined(__aarch64__)
-        #define __c_write linux_aarch64_write
-        #define __c_read linux_aarch64_read
-    #else
-        #error "Unsupported architecture on Linux"
-    #endif
-#elif defined(__APPLE__) // all apple
-    #include <TargetConditionals.h>
-    #if TARGET_OS_OSX
-        #ifdef __x86_64__
-            #define __c_write apple_x86_64_write
-            #define __c_read apple_x86_64_read
-        #elif defined(__i386__)
-            #define __c_write apple_x86_write
-            #define __c_read apple_x86_read
-        #elif defined(__arm__)
-            #define __c_write apple_arm_write
-            #define __c_read apple_arm_read
-        #elif defined(__aarch64__)
-            #define __c_write apple_aarch64_write
-            #define __c_read apple_aarch64_read
-        #else
-            #error "Unsupported architecture on Apple"
-        #endif
-    #else
-        #error "Unsupported Apple platform"
-    #endif
+#if defined(_WIN32) || defined(_WIN64) // Windows
+#ifdef _WIN64
+#define __c_write win_x86_64_write
+#define __c_read win_x86_64_read
+#elif defined(_M_IX86)
+#define __c_write win_x86_write
+#define __c_read win_x86_read
+#elif defined(_M_ARM)
+#define __c_write win_arm_write
+#define __c_read win_arm_read
+#elif defined(_M_ARM64)
+#define __c_write win_arm64_write
+#define __c_read win_arm64_read
 #else
-    #error "Unsupported OS"
+#error "Unsupported architecture on Windows"
+#endif
+#elif defined(__linux__) // Linux
+#ifdef __x86_64__
+#define __c_write linux_x86_64_write
+#define __c_read linux_x86_64_read
+#elif defined(__i386__)
+#define __c_write linux_x86_write
+#define __c_read linux_x86_read
+#elif defined(__arm__)
+#define __c_write linux_arm_write
+#define __c_read linux_arm_read
+#elif defined(__aarch64__)
+#define __c_write linux_aarch64_write
+#define __c_read linux_aarch64_read
+#elif defined(__riscv) && __riscv_xlen == 64
+#define __c_write linux_riscv64_write
+#define __c_read linux_riscv64_read
+#elif defined(__powerpc64__)
+#define __c_write linux_powerpc64_write
+#define __c_read linux_powerpc64_read
+#else
+#error "Unsupported architecture on Linux"
+#endif
+#elif defined(__APPLE__) // macOS
+#include <TargetConditionals.h>
+#if TARGET_OS_OSX
+#if defined(__aarch64__) || defined(__arm__)
+#define __c_write apple_arm64_write
+#define __c_read apple_arm64_read
+#else
+#error "Unsupported architecture on Apple"
+#endif
+#else
+#error "Unsupported Apple platform"
+#endif
+#else
+#error "Unsupported OS"
 #endif
 
-#define STDIN_FD  0
+#define STDIN_FD 0
 #define STDOUT_FD 1
 #define STDERR_FD 2
 
-
-typedef char* string_t;
+typedef char *string_t;
 
 #if defined(__GNUC__) || defined(__clang__)
-    typedef _Float16 __f16_t;
+typedef _Float16 __f16_t;
 #endif
 
 /* int */
@@ -83,21 +83,23 @@ typedef double __f64_t;
 typedef long double __f128_t;
 
 /* ptr */
-typedef void* __ptr_t;
+typedef void *__ptr_t;
 
 /* size */
 typedef unsigned long size_t;
 typedef long ssize_t;
 
-/* linux part
-@
-*/
+/* linux part */
 
 /* all linux write */
 ssize_t linux_x86_write(int fd, const void *buff, size_t count);
 ssize_t linux_x86_64_write(int fd, const void *buff, size_t count);
 ssize_t linux_arm_write(int fd, const void *buff, size_t count);
 ssize_t linux_aarch64_write(int fd, const void *buff, size_t count);
+ssize_t linux_riscv64_write(int fd, const void *buff, size_t count);
+ssize_t linux_powerpc64_write(int fd, const void *buff, size_t count);
+ssize_t linux_mips_write(int fd, const void *buff, size_t count);
+ssize_t linux_sparc64_write(int fd, const void *buff, size_t count);
 
 /* all linux read */
 ssize_t linux_x86_read(int fd, void *buff, size_t count);
@@ -106,44 +108,50 @@ ssize_t linux_arm_read(int fd, void *buff, size_t count);
 ssize_t linux_aarch64_read(int fd, void *buff, size_t count);
 
 /* all windows write */
-//ssize_t win_x86_write(int fd, const void *buff, size_t count);
-//ssize_t win_x86_64_write(int fd, const void *buff, size_t count);
+// ssize_t win_x86_write(int fd, const void *buff, size_t count);
+// ssize_t win_x86_64_write(int fd, const void *buff, size_t count);
 
 /* all windows read */
-//ssize_t win_x86_read(int fd, void *buff, size_t count);
-//ssize_t win_x86_64_read(int fd, void *buff, size_t count);
+// ssize_t win_x86_read(int fd, void *buff, size_t count);
+// ssize_t win_x86_64_read(int fd, void *buff, size_t count);
 
 /* all apple write */
-//ssize_t apple_x86_write(int fd, const void *buff, size_t count);
-//ssize_t apple_x86_64_write(int fd, const void *buff, size_t count);
-//ssize_t apple_arm_write(int fd, const void *buff, size_t count);
-//ssize_t apple_aarch64_write(int fd, const void *buff, size_t count);
+// ssize_t apple_x86_write(int fd, const void *buff, size_t count);
+// ssize_t apple_x86_64_write(int fd, const void *buff, size_t count);
+// ssize_t apple_arm_write(int fd, const void *buff, size_t count);
+// ssize_t apple_aarch64_write(int fd, const void *buff, size_t count);
 
 /* all apple read */
-//ssize_t apple_x86_read(int fd, void *buff, size_t count);
-//ssize_t apple_x86_64_read(int fd, void *buff, size_t count);
-//ssize_t apple_arm_read(int fd, void *buff, size_t count);
-//ssize_t apple_aarch64_read(int fd, void *buff, size_t count);
+// ssize_t apple_x86_read(int fd, void *buff, size_t count);
+// ssize_t apple_x86_64_read(int fd, void *buff, size_t count);
+// ssize_t apple_arm_read(int fd, void *buff, size_t count);
+// ssize_t apple_aarch64_read(int fd, void *buff, size_t count);
 
 /* full write function */
-ssize_t write(int fd, const void *buff, size_t count) {
-    const char *p = (const char*)buff;
+ssize_t write(int fd, const void *buff, size_t count)
+{
+    const char *p = (const char *)buff;
     size_t ttwrite = 0;
-    while (ttwrite < count) {
+    while (ttwrite < count)
+    {
         ssize_t w = __c_write(fd, p + ttwrite, count - ttwrite);
-        if (w <= 0) return (w < 0) ? -1 : ttwrite;
+        if (w <= 0)
+            return (w < 0) ? -1 : ttwrite;
         ttwrite += w;
     }
     return ttwrite;
 }
 
 /* full read function */
-ssize_t read(int fd, void *buff, size_t count) {
-    char *p = (char*)buff;
+ssize_t read(int fd, void *buff, size_t count)
+{
+    char *p = (char *)buff;
     size_t ttread = 0;
-    while (ttread < count) {
+    while (ttread < count)
+    {
         ssize_t r = __c_read(fd, p + ttread, count - ttread);
-        if (r <= 0) return (r < 0) ? -1 : ttread;
+        if (r <= 0)
+            return (r < 0) ? -1 : ttread;
         ttread += r;
     }
     return ttread;
